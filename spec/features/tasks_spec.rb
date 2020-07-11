077adbf3bf725a0b33d1436e20c1a1e73e5b2902
川口美咲 <misaki@arcphilia.co.jp>
2020-07-11 17:39:02 +0900

    [Add]taskのスペックファイルにユーザーがタスクの状態を切り替える追加

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  # ユーザーがタスクの状態を切り替える
+  scenario "user toggles a task", js: true do
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project,
+    name: "Rspec tutorial",
+    owner: user)
+    task = project.tasks.create!(name: "Finish RSpec tutorial")
+
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.Email
+    fill_in "Password", with: user.Password
+    click_button "Log in"
+
+    click_link "RSpec tutorial"
+    check "Finish RSpec tutorial"
+
+    expect(page).to have_css "label#task_#{task.id}.completed"
+    expect(task.reload).to be_completed
+
+    uncheck "Finish RSpec tutorial"
+
+    expect(page).to_not have_css "label#task_#{task.id}.completed"
+    expect(task.reload).to_not be_completed
+  end
 end
