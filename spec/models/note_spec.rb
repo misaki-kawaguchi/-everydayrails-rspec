6a8966258bd4306341c745c1133fe3d7facc1831
川口美咲 <misaki@arcphilia.co.jp>
2020-07-13 21:52:47 +0900

    [Modify]note_spec.rbリファクタリング

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,28 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before do
-    @user = User.create(
-    first_name: "Joe",
-    last_name: "Tester",
-    email: "joetester@example.com",
-    password: "dottle-nouveau-pavilion-tights-furze",
-  )
-
-    @project = @user.projects.create(
-      name: "Test Project",
-    )
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
   it "is valid with a user, project, and message" do
     note = Note.new(
       message: "This is a sample note.",
-      user: @user,
-      project: @project,
+      user: user,
+      project: project,
     )
     expect(note).to be_valid
-end
+  end
 
   # メッセージがなければ無効な状態であること
   it "is invalid without a message" do
@@ -33,26 +23,36 @@ end
 
   # 文字列に一致するメッセージを検索する
   describe "search message for a term" do
-    before do
-      @note1 = @project.notes.create(
-      message: "This is the first note.",
-      user: @user,
+    let!(:note1) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the first note.",
       )
-      @note2 = @project.notes.create(
+    }
+
+    let!(:note2) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "This is the second note.",
-        user: @user,
       )
-      @note3 = @project.notes.create(
+    }
+
+    let!(:note3) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "First, preheat the oven.",
-        user: @user,
       )
-    end
+    }
 
     # 一致するデータが見つかるとき
     context "when a match is found" do
       # 検索文字列に一致するメモを返すこと
       it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
+        expect(Note.search("first")).to include(note1, note3)
+        expect(Note.search("first")).to_not include(note2)
       end
     end
 
@@ -61,6 +61,7 @@ end
       # 空のコレクションを返すこと
       it "returns an empty collection" do
         expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
