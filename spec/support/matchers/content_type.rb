2c4e5b7c51a2ddf2b349e0b4d0d8ebf089031b80
川口美咲 <misaki@arcphilia.co.jp>
2020-07-14 22:51:29 +0900

    [Add]リファクタリング

--- spec/support/matchers/content_type.rb
+++ spec/support/matchers/content_type.rb
@@ -7,6 +7,18 @@ RSpec::Matchers.define :have_content_type do |expected|
     end
   end
 
+  failure_message do |actual|
+    "Expected \"#{content_type(actual.content_type)} " +
+    "(#{actual.content_type})\" to be Content Type " +
+    "\"#{content_type(expected)}\" (#{expected})"
+  end
+
+  failure_message_when_negated do |actual|
+    "Expected \"#{content_type(actual.content_type)} " +
+    "(#{actual.content_type})\" to not be Content Type " +
+    "\"#{content_type(expected)}\" (#{expected})"
+  end
+
   def content_type(type)
     types = {
       html: "text/html",
@@ -15,3 +27,5 @@ RSpec::Matchers.define :have_content_type do |expected|
     types[type.to_sym] || "unknown content type"
   end
 end
+
+RSpec::Matchers.alias_matcher :be_content_type , :have_content_type
