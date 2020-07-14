fc887fcb1a5e99b908721ade7cebbc6cb373ea1d
川口美咲 <misaki@arcphilia.co.jp>
2020-07-14 22:42:16 +0900

    [Modify]リファクタリング

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe TasksController, type: :controller do
     it "responds with JSON formatted output" do
       sign_in @user
       get :show, format: :json,
-        params: { project_id: @project.id, id: @task.id }
-      expect(response.content_type).to eq "application/json"
+      params: { project_id: project.id, id: task.id }
+      expect(response).to have_content_type :json
     end
   end
 
