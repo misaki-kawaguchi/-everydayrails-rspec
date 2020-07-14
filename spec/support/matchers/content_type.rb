4c92db19dc3ca0d6ee6d3f941c3055f54512dbdf
川口美咲 <misaki@arcphilia.co.jp>
2020-07-14 22:50:10 +0900

    [Modify]Content-Type のハッシュを match メソッ ドの外に切り出す

--- spec/support/matchers/content_type.rb
+++ spec/support/matchers/content_type.rb
@@ -1,9 +1,17 @@
 RSpec::Matchers.define :have_content_type do |expected|
   match do |actual|
-    content_types = {
+    begin
+      actual.content_type == content_type(expected)
+    rescue ArgumentError
+      false
+    end
+  end
+
+  def content_type(type)
+    types = {
       html: "text/html",
       json: "application/json",
     }
-    actual.content_type == content_types[expected.to_sym]
+    types[type.to_sym] || "unknown content type"
   end
 end
